fb8028f6cfc899a8a33a28bbd5bd91c438515900
AnnalingamGowsigan <e18115@eng.pdn.ac.lk>
2024-04-08 14:38:29 +0530

    Add Dockerfile and package.json for k8s-web-to-nginx

---
+++ k8s-web-to-nginx/index.mjs
@@ -0,0 +1,37 @@
+// if you want to use the import statement insted of require statement , you need to use the .mjs extension
+
+import express from "express"; // express module is a third party module and it is used to create the web server in nodejs
+import fetch from "node-fetch"; // node-fetch module is a third party module and it is used to make the http requests in nodejs
+import os from "os"; // os module is a core module in nodejs and it is used to get the information about the operating system on which the nodejs is running
+
+const app = express(); // create the express app
+const PORT = 3000; // port number on which the server will run
+
+app.get("/", (req, res) => {
+  try {
+    const eth0 = os.networkInterfaces().eth0;
+    const ipAddress = eth0 ? eth0[0].address : "N/A";
+
+    const helloMessage = `Hello World from Node.js Server running on ${os.hostname()} with IP ${ipAddress}`;
+    console.log(helloMessage);
+    res.send(helloMessage);
+  } catch (error) {
+    console.error("Error occurred while retrieving network interface:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/nginx", async (req, res) => {
+  try {
+    const response = await fetch("http://nginx");
+    const data = await response.text();
+    res.send(data);
+  } catch (error) {
+    console.error("Error occurred while fetching data from Nginx:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
